b291eee3cc9f33a3ebe3b643ddd7ff877b9a2f45
coreymwinter <coreymwinter@gmail.com>
2019-12-09 02:34:13 -0600

    header and single post template overwrites

---
+++ parts/optin-bar.php
@@ -0,0 +1,17 @@
+<div class="fusion-fullwidth fullwidth-box fusion-parallax-none nonhundred-percent-fullwidth non-hundred-percent-height-scrolling fusion-equal-height-columns" style="background-color: rgba(255,255,255,0);background-image: url(&quot;/wp-content/uploads/2019/10/HM04-Thrive-banner_02.png&quot;);background-position: center top;background-repeat: no-repeat;padding-right:30px;padding-left:30px;margin-top: 40px;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;">
+	<div class="fusion-builder-row fusion-row ">
+		<div class="fusion-layout-column fusion_builder_column fusion_builder_column_1_1 fusion-builder-column-2 fusion-one-full fusion-column-first fusion-column-last 1_1" style="margin-top:0px;margin-bottom:0px;">
+			<div class="fusion-column-wrapper" style="padding: 0px; background-position: left top; background-repeat: no-repeat; background-size: cover; min-height: 210px; height: auto;" data-bg-url="">
+				<div class="fusion-column-content-centered" style="min-height: 210px; height: auto;">
+					<div class="fusion-column-content">
+						<div class="fusion-text">
+							<?php echo do_shortcode('[hmca-optin-bar]'); ?>
+						</div>
+					</div>
+				</div>
+				<div class="fusion-clearfix"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
