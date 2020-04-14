5f4f24d20af46dfb47c2fac7df928e4db2c82a33
coreymwinter <coreymwinter@gmail.com>
2020-04-14 16:51:31 -0500

    New font edits

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 <?php
 
 function theme_enqueue_styles() {
+	wp_enqueue_style( 'g_hmca-fonts', 'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600&family=Montserrat:wght@400;600&display=swap', array(), null );
+
+	wp_enqueue_style( 'a_hmca-fonts', 'https://use.typekit.net/tle0ejp.css', array(), null );
+
 	wp_enqueue_style( 'style-guide', get_stylesheet_directory_uri() . '/css/styleguide.css', array(), false);
 	
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'avada-stylesheet' ) );
@@ -12,6 +16,4 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'Avada', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
-
-
 
